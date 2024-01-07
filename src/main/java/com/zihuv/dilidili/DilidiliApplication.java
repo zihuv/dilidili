ea0b69f07c7fa36cca62e3e34be44caf103252e4
zihuv <zihuv@qq.com>
2024-01-07 12:11:51 +0800

    feat: 初始化项目

---
+++ src/main/java/com/zihuv/dilidili/DilidiliApplication.java
@@ -0,0 +1,13 @@
+package com.zihuv.dilidili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilidiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DilidiliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zihuv/dilidili/DilidiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zihuv.dilidili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilidiliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
