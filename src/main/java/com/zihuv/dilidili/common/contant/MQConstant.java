81a5da2e58aa8b3df9937b8e209b4f62a9556fa5
zihuv <zihuv@qq.com>
2024-02-04 17:18:32 +0800

    doc: 修改注释

--- src/main/java/com/zihuv/dilidili/common/contant/MQConstant.java
+++ src/main/java/com/zihuv/dilidili/common/contant/MQConstant.java
@@ -3,17 +3,17 @@ package com.zihuv.dilidili.common.contant;
 public class MQConstant {
 
     /**
-     * 订单服务相关业务 Topic Key
+     * 回复评论 Topic Key
      */
     public static final String RELAY_COMMENT_TOPIC_KEY = "relay_comment_topic_key";
 
     /**
-     * 购票服务创建订单后延时关闭业务 Tag Key
+     * 回复评论 Tag Key
      */
     public static final String RELAY_COMMENT_TAG_KEY = "relay_comment_tag_key";
 
     /**
-     * 购票服务创建订单后延时关闭业务消费者组 Key
+     * 回复评论消费者组 Key
      */
     public static final String RELAY_COMMENT_CG_KEY = "relay_comment_cg";
 
